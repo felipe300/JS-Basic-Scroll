fea1b55814f1bdbac884143886fef0ba3565f366
Felipe Gutierrez <caracoloro@gmail.com>
2020-06-22 14:22:58 -0400

    Setup links

--- app.js
+++ app.js
@@ -7,6 +7,17 @@
 const date = document.querySelector('.date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+    //linksContainer.classList.toggle('show-links')
+    const containerHeight= linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    containerHeight === 0 ? 
+        linksContainer.style.height = `${linksHeight}px`: linksContainer.style.height = 0;
+})
 
 // ********** fixed navbar ************
 
