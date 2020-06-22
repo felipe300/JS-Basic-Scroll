2a1bd1200256a8f88982744c0e817271d0242f6b
Felipe Gutierrez <caracoloro@gmail.com>
2020-06-22 15:09:14 -0400

    Added Scrolling

--- app.js
+++ app.js
@@ -18,8 +18,47 @@ navToggle.addEventListener('click', () => {
     containerHeight === 0 ? 
         linksContainer.style.height = `${linksHeight}px`: linksContainer.style.height = 0;
 })
-
 // ********** fixed navbar ************
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    (scrollHeight > navHeight) ? navbar.classList.add('fixed-nav') : navbar.classList.remove('fixed-nav');
+    (scrollHeight > 500) ? topLink.classList.add('show-link') : topLink.classList.remove('show-link');
+})
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        // prevent defaul value
+        e.preventDefault();
+        // navigate to specific point
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        // calculate heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight= linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+
+        let position = element.offsetTop -navHeight;
+
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
